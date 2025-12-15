590f137fc3c993056aedfbd40a80ec74b4e40431
unknown <gustavoronn@gmail.com>
2025-12-14 21:56:42 -0300

    working

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,15 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
+  //const updatedAt = Date.now();
+  const updatedAt = new Date().toISOString();
+  const query = await database.query(
+    "SELECT (SELECT version()) AS version, (SELECT COUNT (*)::int FROM pg_stat_activity) as pgUsers, (SELECT setting::int FROM pg_settings WHERE name = 'max_connections') AS pgConnections",
+  );
+  console.log(query.rows);
   response.status(200).json({
-    chave: "Tudo Okay",
+    updated_at: updatedAt,
+    query: query.rows,
   });
 }
 export default status;
--- tests/intergration/api/v1/status/get.test.js
+++ tests/intergration/api/v1/status/get.test.js
@@ -1,4 +1,11 @@
 test("GET to /api/v1/status should return to 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json(); //variavel aguarda resposta json
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.query).toBeDefined(); // espera q no corpo da variavel tenha update_at definido
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // verifica se updated_at eh uma data iso
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt); //verifica se as datas coincidem, variavel e corpo
 });
