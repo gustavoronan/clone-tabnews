1ac2b949052ece58d566c5a7414ef032f9e8b04b
unknown <gustavoronn@gmail.com>
2025-12-20 16:22:49 -0300

    error handling and tests refactors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,12 +5,24 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-  console.log(databaseVersionValue);
+
+  const databaseMaxConnResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnValue = databaseMaxConnResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOppenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOppenedConnectionsValue =
+    databaseOppenedConnectionsResult.rows[0].count;
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnValue),
+        oppened_connections: databaseOppenedConnectionsValue,
       },
     },
   });
--- tests/intergration/api/v1/status/get.test.js
+++ tests/intergration/api/v1/status/get.test.js
@@ -9,4 +9,6 @@ test("GET to /api/v1/status should return to 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt); //verifica se as datas coincidem, variavel e corpo
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.oppened_connections).toEqual(1);
 });
