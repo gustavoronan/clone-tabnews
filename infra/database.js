7a9aadcfb0605a2b177a57fdd12086a5b53154ae
unknown <gustavoronn@gmail.com>
2025-12-21 12:30:11 -0300

    handle database ssl connections

--- infra/database.js
+++ infra/database.js
@@ -3,14 +3,15 @@ import { Client } from "pg";
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSGRES_PORT,
+    port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("Variaveis de ambiente:", {
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSGRES_PORT,
+    port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
