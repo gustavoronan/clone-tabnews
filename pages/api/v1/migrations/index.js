c56c1903b00658ca394669dd4b112f63391e8182
unknown <gustavoronn@gmail.com>
2026-01-05 21:39:43 -0300

    working

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,14 +1,27 @@
-import migrationRunner from 'node-pg-migrate'
-import {join} from 'node:path'
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose:true,
-    migrationsTable:"pgmigrations"
-  })
-  response.status(200).json([migrations]);
+  if (request.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    return response.status(200).json(migrations);
+  } else if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    return response.status(200).json(migrations);
+  }
+  response.status(405).end();
 }
--- tests/intergration/api/v1/migrations/get.test.js
+++ tests/intergration/api/v1/migrations/get.test.js
@@ -1,7 +1,16 @@
+import database from "infra/database.js";
+
 test("GET to /api/v1/migrations should return to 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true) 
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+  console.log(responseBody);
+  const jestAmbient = process.env.NODE_ENV;
+  if (jestAmbient == "test") {
+    console.log("O ambiente em que o Jest esta rodando eh de Testes");
+    console.log(process.POSTGRES_DB);
+  }
 });
