c09f7131b1ae670cfa1204519612d163f89a19d4
unknown <gustavoronn@gmail.com>
2026-01-11 12:32:35 -0300

    add: api/v1/migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,27 +1,33 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    return response.status(200).json(migrations);
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   } else if (request.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions, //spread, reutiliza a variavel podendo alterar o valor do atributo
+      dryRun: false,
     });
-    return response.status(200).json(migrations);
+    await dbClient.end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
   }
   response.status(405).end();
 }
--- tests/intergration/api/v1/migrations/post.test.js
+++ tests/intergration/api/v1/migrations/post.test.js
@@ -1,19 +1,21 @@
 import database from "infra/database.js";
-beforeAll(cleanDataBase)
+beforeAll(cleanDataBase);
 async function cleanDataBase() {
-  await database.query("drop schema public cascade; create schema public")
+  await database.query("drop schema public cascade; create schema public");
 }
 test("POST to /api/v1/migrations should return to 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-    const migrationsRowsCount = await database.query(
-   "SELECT COUNT(*)::int as qtMigrations FROM pgmigrations",
-  );
-  console.log(migrationsRowsCount.rows[0]);
-  const migrationName = responseBody.map(item => item.name);
-  expect(migrationName.length).toBeGreaterThan(0);
-  expect(Array.isArray(responseBody)).toBe(true);
+  expect(response1.status).toBe(201);
+  const response1Body = await response1.json();
+
+  expect(Array.isArray(response1Body)).toBe(true);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+  const response2Body = await response2.json();
+  expect(response2Body.length).toBe(0);
 });
