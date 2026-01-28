f4aeabe9dead1a8bd61c848e422344a31ee52975
Gustavo Ronan <gustavoronn@gmail.com>
2026-01-28 15:16:23 -0300

    test: refactoring tests and fixing folder name

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,24 @@
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public");
+});
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    test("Running Pending Migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+
+      const jestAmbient = process.env.NODE_ENV;
+      if (jestAmbient == "test") {
+        console.log("O ambiente em que o Jest esta rodando eh de testes");
+        console.log(process.POSTGRES_DB);
+      }
+    });
+  });
+});
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,36 @@
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public");
+});
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    describe("Running Pending Migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
+        const response1Body = await response1.json();
+        expect(Array.isArray(response1Body)).toBe(true);
+      });
+
+      test("For the Second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+        const response2Body = await response2.json();
+        expect(response2Body.length).toBe(0);
+      });
+    });
+  });
+});
