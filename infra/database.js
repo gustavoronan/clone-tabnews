5512e448752f4a60f089cd14855f27c51f282299
gustavoronan <gustavoronn@gmail.com>
2026-02-04 21:10:50 -0300

    feat: padroniza controller

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors";
 
 async function query(queryObject) {
   let client;
@@ -7,9 +8,10 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro do database.js --------");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
