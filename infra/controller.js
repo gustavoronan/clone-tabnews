b70da21b3df3a5e4c170fe80de71b97b457aff1a
gustavoronan <gustavoronn@gmail.com>
2026-02-11 21:52:38 -0300

    feat: import and use ValidationError

--- infra/controller.js
+++ infra/controller.js
@@ -1,6 +1,13 @@
-import { InternalServerError, MethodNotAllowed } from "infra/errors";
+import {
+  InternalServerError,
+  MethodNotAllowed,
+  ValidationError,
+} from "infra/errors";
 
 function onErrorHandler(error, request, response) {
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error);
+  }
   const publicErrorObject = new InternalServerError({
     statusCode: error.statusCode,
     cause: error,
